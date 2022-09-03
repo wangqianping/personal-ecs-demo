af4bbb3c0137c0634b67b1655261651c3d159ea0
wangqianping <1981517703@qq.com>
2022-09-03 13:55:29 +0800

    Initial commit

---
+++ api-gateway/src/main/java/com/sz/gateway/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.sz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangqianping
+ * @date 2022-08-18
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
---
+++ api-system/src/main/java/com/sz/system/ApiSystemApplication.java
@@ -0,0 +1,16 @@
+package com.sz.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangqianping
+ * @date 2022-08-19
+ */
+@SpringBootApplication
+public class ApiSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSystemApplication.class);
+    }
+}
