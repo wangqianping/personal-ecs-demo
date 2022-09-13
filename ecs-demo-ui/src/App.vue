9abbac0bb787c1416b78cd48177f34171fc3d38f
wangqianping <1981517703@qq.com>
2022-09-14 00:35:58 +0800

    引入element-ui

--- ecs-demo-ui/src/App.vue
+++ ecs-demo-ui/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginForm from './components/LoginForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    LoginForm
+}
 }
 </script>
 
