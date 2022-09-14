b825e3211c643fb609c19e41adb301479258f861
wangqianping <1981517703@qq.com>
2022-09-14 15:15:42 +0800

    登入页完善

--- ecs-demo-ui/src/components/LoginForm.vue
+++ ecs-demo-ui/src/components/LoginForm.vue
@@ -1,23 +1,55 @@
 <template>
-    <div class="login">
-        <h1>test</h1>
-        <el-row>
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
-        </el-row>
-    </div>
+  <div class="login">
+    <el-form :model="user" class="demo-form-inline" label-width="200px">
+      <el-form-item label="账号">
+        <el-input v-model="user.username" placeholder="Please input account"/>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="user.password" type="password" placeholder="Please input password"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登入</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'LoginForm'
+  name: 'LoginForm',
+  data() {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      // console.log(this.user)
+      this.$axios.get("/user/listUser")
+          .then(
+              rsp => {
+                console.log(rsp.data)
+              })
+          .catch(
+              error => {
+                console.log(error)
+              })
+    }
+
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.login {
+  width: 500px;
+  height: 200px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
