27325a1e865173a5d245ea9cad206d108e8d4c19
wangqianping <1981517703@qq.com>
2022-09-14 17:24:34 +0800

    登入页完善

--- ecs-demo-ui/src/main.js
+++ ecs-demo-ui/src/main.js
@@ -3,6 +3,9 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
